1472e468acf8d49b9664a2757af568d9a7bda2eb
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-03-21 23:11:25 +0200

    Remove old demo components

--- demo/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://www.googleapis.com/books/v1/volumes';
--- demo/layout/BaseLayout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const BaseLayout = ({ children }) => {
-  return <div className="layout-container">{children}</div>;
-};
-
-export default BaseLayout;
