e6e9b5b83f73ef20fef8018ad7198bc7ea1d2f74
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-05-01 20:30:43 +0200

    Add JS docs for hooks

--- src/useAPI.js
+++ src/useAPI.js
@@ -3,8 +3,23 @@ import axios from 'axios';
 import hash from 'hash-object';
 
 /**
+ * The object returned by the useAPI hook.
+ * @typedef {Object} useAPIOutput
+ * @property {Object|undefined} data - The data attribute from the axios response.
+ * @property {Object|undefined} response - The axios response.
+ * @property {Object|undefined} error - The axios error object is an error occurs.
+ * @property {boolean} isLoading - Indicates if their is a pending API call.
+ * @property {function(object[]): void} setData - Update the response data object.
+ */
+
+/**
+ * React hook used to make a an API call using axios.
+ *
+ * Allows you to pass an axios config object, for complete control of the request being sent.
+ *
  * @param {string} url - URL that the API call is made to.
- * @param {object} config - Axios config object passed to the axios.request method.
+ * @param {Object} config - Axios config object passed to the axios.request method.
+ * @returns {useAPIOutput} output
  */
 function useAPI(url, config = {}) {
   const [state, setState] = useState({
@@ -30,9 +45,15 @@ function useAPI(url, config = {}) {
       });
   }, [url, configHash]);
 
+  function setData(newData) {
+    // Used to update state from component
+    const newResponse = { ...response, data: newData };
+    setState({ ...state, response: newResponse });
+  }
+
   const { response, error, isLoading } = state;
   const data = response ? response.data : undefined;
-  return { data, response, error, isLoading };
+  return { data, response, error, isLoading, setData };
 }
 
 export default useAPI;
--- src/useParams.js
+++ src/useParams.js
@@ -1,7 +1,24 @@
 import { useState, useMemo } from 'react';
 import debounce from 'lodash.debounce';
 
-export default function useParams(initialParams = {}, debounceDelay=500) {
+/**
+ * The object returned by the useParams hook.
+ * @typedef {Object} useParamsOutput
+ * @property {Object} params - The current params to be used when making an API call.
+ * @property {boolean} isStale - Is their a debounced params update waiting to timeout. (Are we waiting for the user to stop typing)
+ * @property {function(newParams): void} setParams - Function used to set new parameters
+ */
+
+/**
+ * React hook to keep query parameters in state, that can be used for the API calls.
+ *
+ * Includes the ability the debounce an update, which is useful for delaying API calls while the user is typing.
+ *
+ * @param {Object} initialParams - The initial parameters to keep in states
+ * @param {number} debounceDelay=500 - The time to debounce the params update when calling debouncedUpdateParams
+ * @returns {useParamsOutput} output
+ */
+function useParams(initialParams = {}, debounceDelay = 500) {
   const [params, setParams] = useState(initialParams);
   const [isStale, setIsStale] = useState(false);
 
@@ -34,3 +51,5 @@ export default function useParams(initialParams = {}, debounceDelay=500) {
     }
   };
 }
+
+export default useParams;
