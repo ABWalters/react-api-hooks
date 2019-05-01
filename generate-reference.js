4c3c3fe4d71ea19b78d7a2d72dd1ae0317ba4fea
Andrew <abwalters94@gmail.com>
2019-05-01 23:43:20 +0200

    Update generate-reference.js

--- generate-reference.js
+++ generate-reference.js
@@ -1,3 +1,18 @@
 const jsdoc2md = require('jsdoc-to-markdown');
+const fs = require('fs');
 
-jsdoc2md.render({ files: 'src/*.js' }).then(console.log);
+jsdoc2md.render({ files: 'src/useAPI.js' }).then(markdown => {
+  console.log(markdown);
+  fs.writeFileSync(
+    'website/reference/use-api.md',
+    markdown
+  );
+});
+
+jsdoc2md.render({ files: 'src/useParams.js' }).then(markdown => {
+  console.log(markdown);
+  fs.writeFileSync(
+    'website/reference/use-params.md',
+    markdown
+  );
+});
