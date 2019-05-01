e7f05e794eafe6d52dbfc88834450547fa53040a
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-05-01 18:11:58 +0200

    Update constants.js

--- website/constants.js
+++ website/constants.js
@@ -1,2 +1,3 @@
+// https://coolors.co/dff8eb-2e4052-092327-0b5351-00a9a5
 export const booksURL = 'https://www.googleapis.com/books/v1/volumes';
 export const booksInitialParams = { q: 'intitle:react', maxResults: 5 };
