bce25a51a365d154f31f7d9b2072bc126430a823
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-05-02 13:00:29 +0200

    Add cancel token for API calls

--- src/useAPI.js
+++ src/useAPI.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import hash from 'hash-object';
 
+const CancelToken = axios.CancelToken;
+
 /**
  * The object returned by the useAPI hook.
  * @typedef {Object} useAPIOutput
@@ -9,16 +11,22 @@ import hash from 'hash-object';
  * @property {Object|undefined} response - The axios response.
  * @property {Object|undefined} error - The axios error object is an error occurs.
  * @property {boolean} isLoading - Indicates if their is a pending API call.
- * @property {function(object[]): void} setData - Update the response data object.
+ * @property {setDataFunc} setData - Set the response data object.
+ */
+
+/**
+ * `setData` property of `useAPIOutput`
+ * @typedef {function} setDataFunc
+ * @param {Object[]} newData - New data array that overwrites current data.
  */
 
 /**
  * React hook used to make a an API call using axios.
  *
- * Allows you to pass an axios config object, for complete control of the request being sent.
+ * Allows you to pass an [axios config object](https://github.com/axios/axios#request-config), for complete control of the request being sent.
  *
  * @param {string} url - URL that the API call is made to.
- * @param {Object} config - Axios config object passed to the axios.request method.
+ * @param {Object} config={} - Axios config object passed to the axios.request method.
  * @returns {useAPIOutput} output
  */
 function useAPI(url, config = {}) {
@@ -28,30 +36,39 @@ function useAPI(url, config = {}) {
     isLoading: true
   });
 
-  function updateState(stateUpdate) {
-    setState({ ...state, ...stateUpdate });
-  }
-
   const configHash = hash(config);
 
   useEffect(() => {
-    updateState({ isLoading: true });
-    axios(url, config)
+    setState({ ...state, isLoading: true });
+
+    const source = CancelToken.source();
+    axios(url, {
+      ...config,
+      cancelToken: source.token
+    })
       .then(response => {
-        updateState({ error: undefined, response, isLoading: false });
+        setState({ error: undefined, response, isLoading: false });
       })
       .catch(error => {
-        updateState({ error, response: undefined, isLoading: false });
+        if (axios.isCancel(error)) {
+          console.log('Request canceled by cleanup', error.message);
+        } else {
+          setState({ error, response: undefined, isLoading: false });
+        }
       });
+
+    return () => {
+      source.cancel('useEffect cleanup.');
+    };
   }, [url, configHash]);
+
+  const { response, error, isLoading } = state;
 
   function setData(newData) {
     // Used to update state from component
     const newResponse = { ...response, data: newData };
     setState({ ...state, response: newResponse });
   }
-
-  const { response, error, isLoading } = state;
   const data = response ? response.data : undefined;
   return { data, response, error, isLoading, setData };
 }
