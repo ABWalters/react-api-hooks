f2f8fbf219f19f153112eb884a93e3c774b101e0
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-06-09 12:38:45 +0200

    Bug fixes for inf scroll pagination

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,44 @@
-let pageSize = 40;
-
-export function updatePageSize(newPageSize) {
-  pageSize = newPageSize;
-}
-
 /**
- * Default paginator, where the parameter `offset` is used to page through results.
- * @param config {Object} - Axios config object
- * @param paginationState {Object} - Object kept internally to keep track of pagination
- * @return output {Object[]}
- * @return output.updatedConfig {Object} - Config object including pagination changes
- * @return output.updatedPaginationState {Object} - Updated pagination state
+ * Uses offset pagination strategy to page through results in an infite scroller.
+ *
+ * Change the 'offsetParamName' to customize the paginator to your API.
+ *
+ * E.g. you may need to use `offsetParamName='skip'`, `offsetParamName='start'`
+ *
+ * @param {Object} config - Axios config object
+ * @param {Object} paginationState - Object kept internally to keep track of pagination
+ * @param {string} offsetParamName='offset' - Param name used to send offset value to the API
+ * @param {number} pageSize=40 - Increment size for offset.
+ * @return {Object[]} output
+ * @return {Object} output.updatedConfig - Config object including pagination changes
+ * @return {Object} output.updatedPaginationState - Updated pagination state
  */
-export function offsetPaginator(config, paginationState) {
+export function offsetPaginator(
+  config,
+  paginationState,
+  offsetParamName = 'offset',
+  pageSize = 40
+) {
   const { params = {} } = config;
-  const { offset = 0 } = paginationState;
-  const newOffset = offset + pageSize;
-  const updatedParams = { ...params, offset: newOffset };
+  const { offset } = paginationState;
+  const newOffset = (offset !== undefined) ? offset + pageSize : 0;
+  const updatedParams = { ...params, [offsetParamName]: newOffset };
 
   const updatedConfig = { ...config, params: updatedParams };
   const updatedPaginationState = { offset: newOffset };
 
   return [updatedConfig, updatedPaginationState];
+}
+
+/**
+ * Generate your own offset paginator with custom offset param names and page sizes.
+ * @param offsetParamName
+ * @param pageSize
+ * @return {function} paginatorFunc
+ */
+export function getOffsetPaginator(offsetParamName, pageSize) {
+  return (config, paginationState) =>
+    offsetPaginator(config, paginationState, offsetParamName, pageSize);
 }
 
 /**
