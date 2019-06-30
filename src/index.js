824e8c6359595cadb6ead6fbdf6da264195bf0e7
Andrew W - Work Laptop <abwalters94@gmail.com>
2019-06-30 10:03:50 +0200

    Export lib axios instance, add source file to package.json

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as useAPI } from './useAPI';
 export { default as useParams } from './useParams';
 export { default as useInfAPI } from './useInfAPI';
 export { getOffsetPaginator } from './utils';
+export { default as axios } from 'axios'; // Allow access to lib axios instance in order to override defaults
